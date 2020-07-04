de164891f08010b70cd65402569d683c9d9c8e3a
小马 <1421489465@qq.com>
2020-07-04 14:10:09 +0800

    定時脚本測試

--- gitpull.php
+++ gitpull.php
@@ -1,6 +1,6 @@
 <?php
 //寫著玩111
 
-exec("cd /data/gitspace/build;/usr/local/git/bin/git pull /dev/null &2>1");
+system("cd /data/gitspace/build;/usr/local/git/bin/git pull /dev/null &2>1");
 ///再写点什么
 ///
