515d97c0510f4c520e47f629de408d81852e8e17
小马 <1421489465@qq.com>
2020-07-04 14:26:06 +0800

    定時脚本測試

--- gitpull.php
+++ gitpull.php
@@ -1,7 +1,7 @@
 <?php
 //寫著玩11123
 try {
-    system("cd /data/gitspace/build;/usr/local/git/bin/git pull");
+    system("cd /data/gitspace/build && sudo git pull");
     echo "同步成功!";
 }catch (Exception $e){
     echo "同步失败".$e;
