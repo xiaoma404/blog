a10f562f927483f905317438522229df9c9f3469
小马 <1421489465@qq.com>
2020-07-04 14:20:07 +0800

    定時脚本測試

--- gitpull.php
+++ gitpull.php
@@ -1,6 +1,10 @@
 <?php
 //寫著玩11123
-
-system("cd /data/gitspace/build;/usr/local/git/bin/git pull");
+try {
+    system("cd /data/gitspace/build;/usr/local/git/bin/git pull");
+    echo "同步成功!";
+}catch (Exception $e){
+    echo "同步失败".$e;
+}
 ///再写点什么
 ///
