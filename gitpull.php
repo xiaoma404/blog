3ff9754fa2a28a208c0e5832e0dd8da44bac3296
小马 <1421489465@qq.com>
2020-07-04 15:06:29 +0800

    定時脚本測試

--- gitpull.php
+++ gitpull.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-//寫著玩12312366666
+//寫著玩12312366666435
 try {
     $str = shell_exec("cd /data/gitspace/build && sudo /usr/local/git/bin/git pull && echo '1111'");
 //    $str = shell_exec("whoami");
